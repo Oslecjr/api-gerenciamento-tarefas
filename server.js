484be3f3aa89b0a676eb2ba4033fe82663d69a74
Celso Filho <celsofilhomcz@gmail.com>
2024-02-16 19:49:06 -0300

    React e Node funcionando numa mesma port5000

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
+  console.log('Servidor rodando em modo de desenvolvimento');
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
